f4f349958ca2cb027d6f668aac391e9553a86472
saoufi sami <saoufi-sami@hotmail.fr>
2023-05-14 14:45:34 +0200

    ajout de l'exercice 2 avec le screen du fichier executé sur le  terminal

---
+++ Epreuve de la Terre/Terre2.java
@@ -0,0 +1,13 @@
+public class Terre2 {
+
+    public static void main(String... args){
+        Terre2 mp=new Terre2();
+        System.out.println(mp.getClass().getSimpleName()+".java");
+    }
+
+
+}
+
+//je voulai le nom du fichier mais ca ce n'est pas possible comme ca en java car le fichier source peut etre n'importe ou, dans certains cas il n'est meme pas sur la machine
+//J'ai instancie un objet de classe
+//Malgré que j'avais quand même essayé la méthode avec le chemin absolu et l'index, mais ça me ramener seulement au dossier parents
