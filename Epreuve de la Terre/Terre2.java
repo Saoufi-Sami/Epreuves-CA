f1cb4df802e2d8fbdcd13189f4aaba0fc5a8b552
saoufi sami <saoufi-sami@hotmail.fr>
2023-05-14 22:18:03 +0200

    ajout de la premier version de terre 2

--- Epreuve de la Terre/Terre2.java
+++ Epreuve de la Terre/Terre2.java
@@ -1,13 +1,15 @@
 public class Terre2 {
-
-    public static void main(String... args){
-        Terre2 mp=new Terre2();
-        System.out.println(mp.getClass().getSimpleName()+".java");
+    public static void main(String[] args) {
+        String fileName;
+        if (args.length > 0) {
+            fileName = args[0];
+        } else {
+            fileName = "aucun argument";
+        }
+        System.out.println( fileName);
     }
-
-
 }
 
-//je voulai le nom du fichier mais ca ce n'est pas possible comme ca en java car le fichier source peut etre n'importe ou, dans certains cas il n'est meme pas sur la machine
-//J'ai instancie un objet de classe
-//Malgré que j'avais quand même essayé la méthode avec le chemin absolu et l'index, mais ça me ramener seulement au dossier parents
+
+//Ici, pour ne pas utiliser de fonction et ne pas mettre le nom de mon fichier source en dur et bien, je vais passer le nom de ce fichier source en argument
+// lorsque je suis en ligne de commande
