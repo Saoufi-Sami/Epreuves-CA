e10170fea515ce5b42a7eee2fce99429c57418e1
saoufi sami <saoufi-sami@hotmail.fr>
2023-05-15 23:56:12 +0200

    terre 2 modifié

--- Epreuve de la Terre/Terre2.java
+++ Epreuve de la Terre/Terre2.java
@@ -1,15 +1,32 @@
+
 public class Terre2 {
-    public static void main(String[] args) {
-        String fileName;
-        if (args.length > 0) {
-            fileName = args[0];
-        } else {
-            fileName = "aucun argument";
-        }
-        System.out.println( fileName);
+
+    public static void main(String... args){
+        Terre2 mp=new Terre2();
+        System.out.println(mp.getClass().getSimpleName()+".java");
     }
+
+
 }
 
 
-//Ici, pour ne pas utiliser de fonction et ne pas mettre le nom de mon fichier source en dur et bien, je vais passer le nom de ce fichier source en argument
-// lorsque je suis en ligne de commande
+//J'ai instancie un objet de classe
+// je n'est pas utiliser de  fonction
+
+// Contrairement au autres editeur de texte pour avoir le nom du fichier en java  ce n'est pas possible car
+//Le fichier source peut etre n'importe ou, dans certains cas il n'est meme pas sur la machine
+
+//Même en faisant la méthode du chemin absolue, et tu noms de fichiers, cela me ramènes seulement haut de ses parents
+
+
+
+//J'aurais pu utiliser la méthode avec les arguments, mais j'ai préféré la garder pour l'exercice d'après je vous la met
+//quand meme ici
+//        String fileName;
+//        if (args.length > 0) {
+//            fileName = args[0];
+//        } else {
+//            fileName = "aucun argument";
+//        }
+//        System.out.println( fileName);
+//    }
