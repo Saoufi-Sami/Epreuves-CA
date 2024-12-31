f88bd49a6d1b84edde1f6fff2a5b9d61600ed4b4
saoufi sami <saoufi-sami@hotmail.fr>
2024-12-31 17:06:32 +0100

    commentaire rajouté

--- Epreuve de la terre en PHP/Terre1.php
+++ Epreuve de la terre en PHP/Terre1.php
@@ -14,10 +14,12 @@ Attention : votre programme devra utiliser une boucle.
 
 // Boucle pour parcourir les caractères de 'A' à 'Z'
 for ($c = 'A'; $c <= 'Z'; $c++) {
-    // Affichage du caractère suivi d'un espace
+    // Affichage du caractère suivi d'un espace avec la concaténation via le point
     echo $c . " ";
 
-       // Condition pour arrêter la boucle à 'z'
+       // Condition pour arrêter la boucle à 'z'car 
+       //Sans condition d'arrêt, PHP continue à incrémenter les lettres après 'Z' selon l'ordre ASCII, 
+       //où 'A' est à 65, 'Z' est à 90, et ensuite 'AA' commence à 97, générant des séquences comme 'AA', 'AB', etc., de manière infinie.
     if ($c == 'Z') break;
 }
 
