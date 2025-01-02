f0a49f4c9bc35f0ec8b4a2c01b7fed2db43257f4
saoufi sami <saoufi-sami@hotmail.fr>
2025-01-02 15:00:33 +0100

    commentaire rajouté

--- Epreuve de la terre en PHP/Terre5.php
+++ Epreuve de la terre en PHP/Terre5.php
@@ -25,15 +25,20 @@ Fonctions interdites:
 */
 
 
-// Vérifie si aucun argument n'est passé (seulement le nom du script)
+// Vérifie si aucun argument n'est passé (seulement le nom du script en gros si ya un agrument c'est seuelement le script)
 if ($argc == 1) {
     echo "Tu ne me la mettras pas à l’envers il n’y a rien.\n";
 } 
-// Vérifie si le nombre total d'arguments n'est pas exactement 2
+// Vérifie si le nombre total d'arguments n'est pas exactement 2 en gros si c'est 2 c que ya un le script + 1 argument donc c'est ok  mais la on regarde si c different de 2 donc le scprit + largument c'est que tu en a mis +.
 else if ($argc != 2) {
     echo "Il faut passer un seul argument de type entier .\n";
 } 
-// Vérifie si l'argument n'est pas un entier valide
+
+// la premiere couche c'est is_numeric() qui vérifie si la valeur est un nombre valide (entier ou flottant),
+// donc elle accepte des chiffres avec des virgules comme "6.5", "-3.2" mais aussi des chaînes numériques comme "123" mais pa de chaines de characteres.
+
+// la deuxieme couche c'est filter_var() avec FILTER_VALIDATE_INT qui va encore plus filtrer et  permet de vérifier que la valeur est **un entier valide**
+// Cela signifie qu'il doit s'agir d'un nombre entier sans virgule ni caractère non numérique.
 else if (!is_numeric($argv[1]) || filter_var($argv[1], FILTER_VALIDATE_INT) === false) {
     echo "Tu ne me la mettras pas à l’envers avec ton mot je veux un chiffre.\n";
 } 
@@ -42,10 +47,11 @@ else {
     $nombre = (int)$argv[1];
     
     // Vérifier si le nombre est pair ou impair
-    if ($nombre % 2 == 0) {
-        echo "pair\n";
-    } else {
-        echo "impair\n";
-    }
+ // Le modulo (%) permet de diviser le nombre par 2 et de vérifier le reste
+if ($nombre % 2 == 0) { // Si le reste est égal à 0, le nombre est pair
+    echo "pair\n"; // Affiche "pair"
+} else { // Si le reste n'est pas 0, le nombre est impair
+    echo "impair\n"; // Affiche "impair"
+}
 }
 ?>
